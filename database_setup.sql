d475643a580311dfd97a3af0595aa977c795e905
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2021-02-05 19:42:24 -0600

    added alignment table, created character seeder, starting seeding characters

--- database_setup.sql
+++ database_setup.sql
@@ -1,5 +1,11 @@
 use adventuregame_db;
 
+create table if not exists alignment_list(
+    id int unsigned auto_increment,
+    type varchar(15) not null,
+    primary key (id)
+);
+
 create table if not exists items_list
 (
     id          int unsigned auto_increment,
@@ -19,15 +25,17 @@ create table if not exists followers_list
 create table if not exists base_characters
 (
     id        int unsigned auto_increment,
+    name      varchar(50)  not null,
     life      int unsigned not null,
     fate      int unsigned not null,
     strength  int unsigned not null,
     craft     int unsigned not null,
     gold      int unsigned not null,
     alignment int unsigned not null,
-    objects   int unsigned not null,
-    followers int unsigned not null,
+    objects   int unsigned,
+    followers int unsigned,
     PRIMARY KEY (id),
+    foreign key (alignment) references alignment_list(id),
     foreign key (objects) references items_list (id),
     foreign key (followers) references followers_list (id)
 );
